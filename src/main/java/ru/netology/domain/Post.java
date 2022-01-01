8bc1197b8b35463377ca175440bb49352054b0cf
Timofei <t.a.maksimenko@ya.ru>
2022-01-01 21:42:38 +0300

    added classes like vk official description

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,16 +1,34 @@
 package ru.netology.domain;
+import ru.netology.domain.description.*;
 
 public class Post {
     private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int signerId;
     private String name;
-    private String description;
-    private String nameOfAuthor;
+    private String text;
+    private String postType;
+    private int replyOwnerId;
+    private int replyPostId;
     private String img;
-    private long date;
-    private long time;
+    private int date;
     private int numOfView;
-    private int numOfLikes;
-    private int numOfRepost;
-    private boolean favourites;
+    private boolean isFavorite;
+    private boolean friendsOnly;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean postponedId;
+    private Comments comments;
+    private Copyright copyright;
+    private Donut donut;
+    private Geo geo;
+    private Likes likes;
+    private Reports reports;
+
 
 }
