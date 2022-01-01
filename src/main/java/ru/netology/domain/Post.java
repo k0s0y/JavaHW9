1d7a056c9b5c2b8abd747d3a86a65a092f39820a
Timofei <t.a.maksimenko@ya.ru>
2022-01-01 20:48:54 +0300

    innitial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private String nameOfAuthor;
+    private String img;
+    private long date;
+    private long time;
+    private int numOfView;
+    private int numOfLikes;
+    private int numOfRepost;
+    private boolean favourites;
+
+}
